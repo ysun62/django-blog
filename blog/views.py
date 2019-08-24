50ad89bfae1e0a3be741b319b8015c7d61600a74
Yang Sun <ysun62@ucsc.edu>
2019-08-24 15:22:20 -0700

    first commit

---
+++ blog/views.py
@@ -0,0 +1,98 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from .models import Post
+
+# Dummy data
+# posts = [
+#     {
+#         'author': 'Yang Sun',
+#         'title': 'Post1',
+#         'content': 'learning Django',
+#         'date': '7/30/2019'
+#     },
+#     {
+#         'author': 'MingYang Sun',
+#         'title': 'Post2',
+#         'content': 'I love CF',
+#         'date': '7/31/2019'
+#     }
+# ]
+
+def home(request):
+    context = {
+        'posts': Post.objects.all(),
+        'title': 'Home'
+    }
+
+    return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date']
+    paginate_by = 6
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date']
+    paginate_by = 6
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
